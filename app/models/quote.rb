12f5a4eb61f22bc5cc835cb49b25594cef68801d
Isa <pdo.isabelle@gmail.com>
2024-03-16 11:01:06 -0300

    FEAT: Conexão com o banco de dados e criação de modelos Quote e Tag

---
+++ app/models/quote.rb
@@ -0,0 +1,8 @@
+class Quote
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :quote, type: String
+  field :author, type: String
+  field :author_about, type: String
+  has_and_belongs_to_many :tags
+end
