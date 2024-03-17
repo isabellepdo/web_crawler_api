e3305b29068d2d1fb9a10c123f14c8108415d4be
Isa <pdo.isabelle@gmail.com>
2024-03-17 11:07:43 -0300

    Feat: Salvando no banco de dados busca de quotes por tag

--- app/models/quote_crawler.rb
+++ app/models/quote_crawler.rb
@@ -2,22 +2,28 @@ require 'nokogiri'
 require 'open-uri'
 
 class QuoteCrawler
-  def initialize(tag)
-    @tag = tag
-    @base_url = 'http://quotes.toscrape.com/tag/'
-  end
+	def initialize(tag)
+		@tag = tag
+		@base_url = 'http://quotes.toscrape.com/tag/'
+	end
 
-  def crawl
-    url = "#{@base_url}#{@tag}/"
-    doc = Nokogiri::HTML(URI.open(url))
+	def crawl
+		url = "#{@base_url}#{@tag}/"
+		doc = Nokogiri::HTML(URI.open(url))
 
-    quotes = doc.css('.quote').map do |quote_node|
-      {
-        text: quote_node.css('.text').text.strip,
-        author: quote_node.css('.author').text.strip
-      }
-    end
+		doc.css('.quote').each do |quote_node|
+			quote_text = quote_node.css('.text').text.strip
+			author_name = quote_node.css('.author').text.strip
+			author_about_link = quote_node.at_css('span > a')['href']
 
-    quotes.each { |quote| puts "#{quote[:text]} - #{quote[:author]}" }
-  end
+			quote = Quote.find_or_initialize_by(quote: quote_text, author: author_name, author_about: author_about_link)
+
+			if quote.new_record? 
+				quote.save
+
+				tag = Tag.find_or_create_by(name: @tag)
+				quote.tags << tag
+			end
+		end
+	end
 end
