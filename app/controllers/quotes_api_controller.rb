f869f375de1d2b953850daa4214186d0fa504e8c
Isa <pdo.isabelle@gmail.com>
2024-03-19 10:20:26 -0300

    Refactor: Ajustes para utilização do token

--- app/controllers/quotes_api_controller.rb
+++ app/controllers/quotes_api_controller.rb
@@ -1,4 +1,6 @@
 class QuotesApiController < ApplicationController
+	before_action :authenticate_user
+
 	def get_quotes_by_tag
 		tag = params[:tag]
 		use_tag = Tag.find_by(name: tag)
@@ -12,4 +14,15 @@ class QuotesApiController < ApplicationController
 
 		render json: quotes
 	end
+
+	private
+
+  def authenticate_user
+    token = request.headers['Authorization']&.split(' ')&.last
+    decoded_token = AuthService.decode_token(token)
+
+    unless decoded_token
+      render json: { error: 'Token inválido' }, status: :unauthorized
+    end
+  end
 end
